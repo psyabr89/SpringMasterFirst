e20a797de9a64b76dcdff79070daf859fa0ddcd9
kartashov.adm <igorek6500@gmaol.com>
2023-11-22 14:36:52 +0300

    Comments for all positions

--- src/main/java/ru/lex/mirelwedding/Interfaces/Methods.java
+++ src/main/java/ru/lex/mirelwedding/Interfaces/Methods.java
@@ -1,7 +1,6 @@
 package ru.lex.mirelwedding.Interfaces;
 
 public interface Methods { //Данный интерфейс не нужен. Его следует удалить
-    //Думаю все же стоит его удалить, этот интерфейс.
     public void AddUsers();
     public void DeleteUsers();
     public void SearchUsers();
