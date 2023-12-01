089d4cfe54fc3452fa1fb6c35cc0b2f3a75f9bf2
kartashov.adm <igorek6500@gmaol.com>
2023-12-01 10:50:59 +0300

    Correct for launch

--- src/main/java/ru/lex/mirelwedding/Controllers/MainController.java
+++ src/main/java/ru/lex/mirelwedding/Controllers/MainController.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.lex.mirelwedding.Entities.Users;
 import ru.lex.mirelwedding.Repositories.UsersRepository;
 
 @RestController
